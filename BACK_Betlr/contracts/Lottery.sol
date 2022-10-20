173d6d96cfb13b5fd3faa36e8261cdc4f5a3539d
Carlos Vouking <carlosvouking@gmail.com>
2022-10-20 12:09:18 +0200

    fulfillRandomWords(): and paying back the random winner implemented

--- BACK_Betlr/contracts/Lottery.sol
+++ BACK_Betlr/contracts/Lottery.sol
@@ -6,6 +6,7 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Lottery__InsufficientParticipationFees();
+error Lottery__WinnerTransferFundsFailed();
 
 contract Lottery is VRFConsumerBaseV2 {
     /**State variable */
@@ -21,9 +22,13 @@ contract Lottery is VRFConsumerBaseV2 {
     uint32 private immutable i_callbackGasLimit;
     uint32 private constant NUM_WORDS = 1;
 
+    // Rafffle related state variables
+    address private s_recentRandomWinner; // voorlopig empty = no random winner momenteel
+
     /**Events */
     event LotteryEnter(address indexed player);
     event RequestedLotteryWinner(uint256 indexed requestId);
+    event randomWinnerPicked(address indexed winnerPicked);
 
     // initializing items at contract deployment....vrfCoordinatorV2= address consumer contract from Remix to the subscription
     constructor(
@@ -66,10 +71,32 @@ contract Lottery is VRFConsumerBaseV2 {
     }
 
     // computed from VRFConsumerBaseV2.sol
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {}
+    function fulfillRandomWords(
+        uint256, /*requestId*/
+        uint256[] memory randomWords
+    ) internal override {
+        /*
+           % modulo scenario to get the random winner: 
+            // s_players = [56, 789, 402, 78, 3254, 202, 91, 6587, 80, 65]
+            // random number to be picked: 402
+            // to goal of the modulo is to find the index of the random nmber to be picked
+            randomWords[0] % s_players.length = indexRandomNumber
+                 402       %        10        =       2
+        */
+        uint256 indexOfRandomWinner = randomWords[0] % s_participants.length;
+        address payable recentRandomWinner = s_participants[indexOfRandomWinner]; // this is our very verifiably random winner.
+        s_recentRandomWinner = recentRandomWinner;
+
+        // send the money to the winner
+        bool success;
+        if (!success) {
+            revert Lottery__WinnerTransferFundsFailed();
+        } else {
+            (success, ) = recentRandomWinner.call{value: address(this).balance}(" ");
+        }
+        // write every random winner to the event log, so that we can query them previous winners at any time
+        emit randomWinnerPicked(recentRandomWinner);
+    }
 
     /** VIEW & | PURE functions */
     // read fee
@@ -80,5 +107,10 @@ contract Lottery is VRFConsumerBaseV2 {
     // read a specific participant
     function getParticipant(uint256 index) public view returns (address) {
         return s_participants[index];
+    }
+
+    // read the recent random winner
+    function getRecentRandomWinner() public view returns (address) {
+        return s_recentRandomWinner;
     }
 }
